580c4f80c63604e59f5ec823cb91abfc7f9209aa
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-01-08 20:43:02 -0800

    prettier

--- src/routes/r/[slug].svelte
+++ src/routes/r/[slug].svelte
@@ -1,270 +1,255 @@
 <script>
-  import Icon from 'fa-svelte';
-  import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay'
-  import { faPause } from '@fortawesome/free-solid-svg-icons/faPause'
-  import { faCog as faSettings } from '@fortawesome/free-solid-svg-icons/faCog'
-  import { faCloudDownloadAlt as faDownload } from '@fortawesome/free-solid-svg-icons/faCloudDownloadAlt'
-  import { faStar as faFav } from '@fortawesome/free-solid-svg-icons/faStar'
-  import { faStar as faUnFav} from '@fortawesome/free-regular-svg-icons/faStar'
+import Icon from "fa-svelte";
+import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+import { faPause } from "@fortawesome/free-solid-svg-icons/faPause";
+import { faCog as faSettings } from "@fortawesome/free-solid-svg-icons/faCog";
+import { faCloudDownloadAlt as faDownload } from "@fortawesome/free-solid-svg-icons/faCloudDownloadAlt";
+import { faStar as faFav } from "@fortawesome/free-solid-svg-icons/faStar";
+import { faStar as faUnFav } from "@fortawesome/free-regular-svg-icons/faStar";
 
-  import { onMount } from  'svelte';
-  import { stores } from '@sapper/app'
+import { onMount } from "svelte";
+import { stores } from "@sapper/app";
 
-  import { get_posts, format } from '../_utils'
+import { get_posts, format } from "../_utils";
 
-  const { page } = stores()
-  const { slug } = $page.params;
+const { page } = stores();
+const { slug } = $page.params;
 
-  let data
-  let posts = []
-  let displayposts = []
-  let res
-  let after
-  let uiVisible = true
-  let selected = 0
+let data;
+let posts = [];
+let displayposts = [];
+let res;
+let after;
+let uiVisible = true;
+let selected = 0;
 
-  let downloadstr = ''
-  let autoplaystr = ''
-  let over18str = ''
+let downloadstr = "";
+let autoplaystr = "";
+let over18str = "";
 
-  let autoplay
-  let autoplayinterval = 3
-  let autoplaytimer
+let autoplay;
+let autoplayinterval = 3;
+let autoplaytimer;
 
-  let saferesults = false
+let saferesults = false;
 
-  let currpost = {title: 'Loading ..'}
-  let nexturls = []
+let currpost = { title: "Loading .." };
+let nexturls = [];
 
-  let index = 0
+let index = 0;
 
-  async function loadMore() {
-    if (!after) return;
+async function loadMore() {
+  if (!after) return;
 
-    let newposts
+  let newposts;
 
-    ({posts: newposts, after, ...res} = await get_posts(`https://reddit.com/r/${slug}.json?after=${after}`));
-    console.log("loadMore", res.res.ok)
+  ({ posts: newposts, after, ...res } = await get_posts(
+    `https://reddit.com/r/${slug}.json?after=${after}`
+  ));
+  console.log("loadMore", res.res.ok);
 
-    posts = [...posts, ...newposts]
+  posts = [...posts, ...newposts];
+}
+
+onMount(async () => {
+  ({ posts, after, ...res } = await get_posts(
+    `https://reddit.com/r/${slug}.json`
+  ));
+
+  // Start autoplay by default
+  startAutoPlay();
+});
+
+function startAutoPlay() {
+  autoplaytimer = setInterval(() => next(), autoplayinterval * 1000);
+
+  autoplay = true;
+}
+
+function stopAutoPlay() {
+  clearInterval(autoplaytimer);
+  autoplay = false;
+}
+
+function stopAndStartAutoPlay() {
+  stopAutoPlay();
+  startAutoPlay();
+}
+
+function toggleAutoPlay() {
+  if (autoplay) {
+    stopAutoPlay();
+  } else {
+    startAutoPlay();
   }
+}
 
-  onMount(async () => {
-    ({ posts, after, ...res} = await get_posts(`https://reddit.com/r/${slug}.json`))
+let renderVideo = true;
 
-    // Start autoplay by default
-    startAutoPlay()
+$: {
+  reMountVideo(currpost.preview);
+}
 
+function reMountVideo() {
+  renderVideo = false;
+  setTimeout(() => (renderVideo = true), 0);
+}
+
+$: {
+  selected = displayposts.filter(item => item.selected == true).length;
+
+  if (!selected) {
+    downloadstr = `nothing to download`;
+  } else if (selected == 1) {
+    downloadstr = `download ${selected} file`;
+  } else {
+    downloadstr = `download ${selected} files`;
   }
-  )
+  autoplaystr = `autoplay is ${autoplay ? "on" : "off"}`;
+  over18str = `nsfw is ${saferesults ? "off" : "on"}`;
+}
 
-  function startAutoPlay() {
+$: {
+  if (displayposts[index]) {
+    currpost = JSON.parse(JSON.stringify(displayposts[index]));
 
-      autoplaytimer = setInterval(() => {
+    nexturls = displayposts.slice(index + 1, index + 4);
+  } else {
+    if (res && res.res.ok) {
+      // No media found
+      currpost = { title: "Nothing to show" };
+    } else if (res && !res.res.ok) {
+      // Invalid subreddit
+      currpost = { title: "Error" };
+    } else {
+      // Default
+      currpost = { title: "Loading .." };
+    }
 
-        index += 1
-
-      }, autoplayinterval * 1000)
-
-    autoplay = true
-
+    nexturls = [];
   }
+}
 
-
-  function stopAutoPlay() {
-    clearInterval(autoplaytimer)
-    autoplay = false
+$: {
+  if (saferesults) {
+    displayposts = posts.filter(item => item.over18 == false);
+  } else {
+    displayposts = posts;
   }
+}
 
-  function stopAndStartAutoPlay() {
-    stopAutoPlay()
-    startAutoPlay()
+function goto(i) {
+  index = i;
+
+  if (displayposts.length - index === 1) {
+    loadMore();
   }
+}
 
-  function toggleAutoPlay() {
-    if(autoplay) {
-      stopAutoPlay()
-    }else {
-      startAutoPlay()
+function next() {
+  index += 1;
+
+  if (displayposts.length - index === 3) {
+    loadMore();
+  }
+}
+
+function prev() {
+  if (index === 0) return;
+  index -= 1;
+
+  if (displayposts.length - index === 3) {
+    loadMore();
+  }
+}
+
+function toggleVisiblity() {
+  uiVisible = !uiVisible;
+}
+
+async function downloadFiles() {
+  //let res = await fetch('/download')
+  //let pagehtml = await res.text()
+
+  let win = window.open("", "title");
+
+  for (const [i, post] of displayposts.entries()) {
+    if (post.selected) {
+      //pagehtml += `<img src="${post.url}" />`
+
+      // We need displayposts[i].selected here to make this change reactive
+      displayposts[i].selected = false;
     }
   }
 
-  let renderVideo = true
+  //console.log(win.document.body, pagehtml)
+  win.document.body.innerHTML =
+    '<img src="https://i.redd.it/hjt5at4l2f941.jpg" />';
+}
 
-  $: {reMountVideo(currpost.preview)}
+function openMedia() {
+  window.open(currpost.url, "_blank");
+}
 
-  function reMountVideo() {
-    renderVideo = false
-    setTimeout(() => renderVideo = true, 0)
+function openSubReddit() {
+  window.open(`https://reddit.com/${currpost.permalink}`, "_blank");
+}
+
+function openSubRedditOld() {
+  window.open(`https://old.reddit.com/${currpost.permalink}`, "_blank");
+}
+
+function keydown(event) {
+  console.log(event.keyCode);
+
+  // x
+  if (event.keyCode == 88) {
+    console.log(displayposts[index]);
+    displayposts[index].selected = !displayposts[index].selected;
   }
 
-  $ : {
-    selected = displayposts.filter((item) => item.selected == true).length
-
-    if (!selected) {
-      downloadstr = `nothing to download`
-    }else if (selected == 1){
-      downloadstr = `download ${selected} file`
-    }else {
-      downloadstr = `download ${selected} files`
-    }
-    autoplaystr = `autoplay is ${autoplay ? "on": "off"}`
-    over18str = `nsfw is ${saferesults ? "off" : "on"}`
+  if (event.ctrlKey) {
+    return;
   }
 
-
-  $ : {
-
-    if (displayposts[index]) {
-      currpost = JSON.parse(JSON.stringify(displayposts[index]))
-
-      nexturls = displayposts.slice(index+1, index+4)
-
-    }
-    else {
-
-      if (res && res.res.ok) {
-
-        // No media found
-        currpost = {title: 'Nothing to show'}
-      }else if (res && !res.res.ok){
-
-        // Invalid subreddit
-        currpost = {title: 'Error'}
-      }else {
-
-        // Default
-        currpost = {title: 'Loading ..'}
-      }
-
-      nexturls = []
-    }
-
+  // r
+  if (event.keyCode == 82) {
+    openSubReddit();
   }
 
-  $ : {
-    if (saferesults) {
-       displayposts = posts.filter((item) => item.over18 == false)
-    }else {
-       displayposts = posts
-    }
+  // o
+  if (event.keyCode == 79) {
+    openSubRedditOld();
   }
 
-  function goto(i) {
-
-    index = i
-
-    if((displayposts.length - index) === 1)  {
-      loadMore()
-    }
-
-
+  // i
+  if (event.keyCode == 73) {
+    openMedia();
   }
 
-  function next() {
-    index += 1
-
-    if((displayposts.length - index) === 3)  {
-      loadMore()
-    }
+  // Up Arrow
+  if (event.keyCode == 38) {
+    toggleVisiblity();
   }
 
-  function prev() {
-    if (index === 0) return;
-    index -= 1
-
-    if((displayposts.length - index) === 3)  {
-      loadMore()
-    }
+  // Left Arrow, a, k
+  if (event.keyCode == 37 || event.keyCode == 65 || event.keyCode == 75) {
+    prev();
   }
-
-  function toggleVisiblity() {
-    uiVisible = !uiVisible
+  // Right Arrow, d, j, Space
+  else if (
+    event.keyCode == 39 ||
+    event.keyCode == 68 ||
+    event.keyCode == 74 ||
+    event.keyCode == 32
+  ) {
+    next();
   }
-
-  async function downloadFiles() {
-
-
-    //let res = await fetch('/download')
-    //let pagehtml = await res.text()
-
-    let win = window.open("", 'title');
-
-    for(const [i, post] of displayposts.entries()) {
-
-      if (post.selected) {
-        //pagehtml += `<img src="${post.url}" />`
-
-        // We need displayposts[i].selected here to make this change reactive
-        displayposts[i].selected = false
-      }
-    }
-
-    //console.log(win.document.body, pagehtml)
-    win.document.body.innerHTML = '<img src="https://i.redd.it/hjt5at4l2f941.jpg" />'
-  }
-
-  function openMedia() {
-    window.open(currpost.url, '_blank')
-  }
-
-  function openSubReddit() {
-    window.open(`https://reddit.com/${currpost.permalink}`, '_blank')
-  }
-
-  function openSubRedditOld() {
-    window.open(`https://old.reddit.com/${currpost.permalink}`, '_blank')
-  }
-
-  function keydown(event) {
-    console.log(event.keyCode)
-
-    // x
-    if (event.keyCode == 88) {
-      console.log(displayposts[index])
-      displayposts[index].selected = !displayposts[index].selected
-    }
-
-    if (event.ctrlKey) {
-      return
-    }
-
-    // r
-    if (event.keyCode == 82) {
-      openSubReddit()
-    }
-
-    // o
-    if (event.keyCode == 79) {
-      openSubRedditOld()
-    }
-
-    // i
-    if (event.keyCode == 73) {
-      openMedia()
-    }
-
-    // Up Arrow
-    if (event.keyCode == 38) {
-      toggleVisiblity()
-    }
-
-    // Left Arrow, a, k
-    if (event.keyCode == 37  || event.keyCode == 65 || event.keyCode == 75) {
-      prev()
-    }
-    // Right Arrow, d, j, Space
-    else if (event.keyCode == 39 || event.keyCode == 68 || event.keyCode == 74 || event.keyCode == 32) {
-      next()
-    }
-  }
-
-
+}
 
 </script>
 
 <style lang="sass">
-
 $yellow: #f9ab00
 
 $text-color: #fafafa
@@ -283,8 +268,6 @@ $over18-border-color: #ea4335
   display: grid
   justify-items: center
   align-items: center
-
-
 
   .hero
     height: 100vh
@@ -317,7 +300,6 @@ $over18-border-color: #ea4335
 
       &.selected
         color: $selected-color
-
 
     .goto
       z-index: 5
@@ -417,7 +399,6 @@ $over18-border-color: #ea4335
             background-color: rgba(255, 255, 255, 0.2)
             border-bottom: 3px solid $accent-color !important
 
-
         img.small
           z-index: 10
           width: 0px
@@ -439,7 +420,6 @@ $over18-border-color: #ea4335
           height: 100px
           opacity: 1
 
-
     .control
       position: absolute
       height: 100%
@@ -457,7 +437,6 @@ $over18-border-color: #ea4335
         right: 0
         width: 90%
         //background-color: rgba(138, 255, 233, 0.38)
-
 
     .image
       height: 100%
@@ -483,8 +462,6 @@ $over18-border-color: #ea4335
     .hero
       .goto
         padding: 1rem
-
-
 
 .tooltip
   position: relative
@@ -533,9 +510,6 @@ $over18-border-color: #ea4335
       visibility: visible
       opacity: 1
 
-
-
-
 </style>
 
 <svelte:window on:keydown={keydown}/>
@@ -543,41 +517,56 @@ $over18-border-color: #ea4335
 <template lang="pug">
 .wrapper
   .hero
-    .control.prev(on:click='{prev}')
+    .control.prev(on:click="{prev}")
     .title(class:hide="{uiVisible == false}", class:selected="{currpost.selected}") {currpost.title}
     .settings(class:hide="{uiVisible == false}")
-      Icon(icon='{faSettings}')
+      Icon(icon="{faSettings}")
     +if('currpost.is_image')
       .image(style="background-image: url('{currpost.preview.img.default}')")
       +elseif('currpost.is_video && renderVideo')
-        video.video(autoplay loop playsinline muted)
+        video.video(autoplay, loop, playsinline, muted)
           +if('currpost.preview.vid.webm')
-            source(src='{currpost.preview.vid.webm}')
+            source(src="{currpost.preview.vid.webm}")
           +if('currpost.preview.vid.mp4')
-            source(src='{currpost.preview.vid.mp4}')
+            source(src="{currpost.preview.vid.mp4}")
           //img(alt="foo", src='{currpost.preview.vid.gif}')
 
-    .control.next(on:click='{next}')
+    .control.next(on:click="{next}")
     +if('displayposts.length')
       .goto(class:hide="{uiVisible == false}")
-        span.btn.playpause.tooltip(data-tooltip="{autoplaystr}", class:play='{autoplay}', on:click='{function(){toggleAutoPlay()}}')
-          Icon(icon='{autoplay ? faPause : faPlay}')
-        span.btn.download.tooltip(on:click='{function(){downloadFiles()}}', data-tooltip="{downloadstr}", class:dlready="{selected}")
-          Icon(icon='{faDownload}')
-        span.btn.over18.tooltip(data-tooltip="{over18str}", class:saferesults='{saferesults}', on:click='{function(){saferesults = !saferesults}}')
+        span.btn.playpause.tooltip(
+          data-tooltip="{autoplaystr}",
+          class:play="{autoplay}",
+          on:click="{function(){toggleAutoPlay()}}"
+        )
+          Icon(icon="{autoplay ? faPause : faPlay}")
+        span.btn.download.tooltip(
+          on:click="{function(){downloadFiles()}}",
+          data-tooltip="{downloadstr}",
+          class:dlready="{selected}"
+        )
+          Icon(icon="{faDownload}")
+        span.btn.over18.tooltip(
+          data-tooltip="{over18str}",
+          class:saferesults="{saferesults}",
+          on:click="{function(){saferesults = !saferesults}}"
+        )
           p nsfw
         +each('displayposts as post, i')
-          span(class:selected='{displayposts[i].selected}', class:over18='{displayposts[i].over18}', on:click="{function(){goto(i)}}")
+          span(
+            class:selected="{displayposts[i].selected}",
+            class:over18="{displayposts[i].over18}",
+            on:click="{function(){goto(i)}}"
+          )
             img.small(alt="foo", src="{displayposts[i].preview.img.default}")
             p.small(class:curr="{index === i}") {i+1}
   .prefetch
     +each('nexturls as nexturl')
-      img(alt='prefetch', src='{nexturl.preview.img.default}')
+      img(alt="prefetch", src="{nexturl.preview.img.default}")
       +if('nexturl.is_video')
         video
           +if('nexturl.preview.vid.webm')
-            source(src='{nexturl.preview.vid.webm}')
+            source(src="{nexturl.preview.vid.webm}")
           +if('nexturl.preview.vid.mp4')
-            source(src='{nexturl.preview.vid.mp4}')
-
+            source(src="{nexturl.preview.vid.mp4}")
 </template>
