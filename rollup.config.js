af9a17085dddaed711666ab029ee09b793717fb2
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-01-19 14:21:23 -0800

    Upgrading rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,126 +1,139 @@
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import svelte from "rollup-plugin-svelte";
-import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import config from "sapper/config/rollup.js";
-import pkg from "./package.json";
-import sveltePreprocess from "svelte-preprocess";
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
+import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
 
+import sveltePreprocess from "svelte-preprocess";
 import typescript from "rollup-plugin-typescript2";
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
-
-const onwarn = (warning, onwarn) =>
-  (warning.code === "CIRCULAR_DEPENDENCY" &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
-  onwarn(warning);
-const dedupe = (importee) =>
-  importee === "svelte" || importee.startsWith("svelte/");
 
 const preprocess = sveltePreprocess({
   pug: true,
   sass: true
 });
 
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	onwarn(warning);
+
 export default {
-  client: {
-    input: config.client.input(),
-    output: config.client.output(),
-    plugins: [
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
       typescript(),
-      replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode)
-      }),
-      svelte({
-        preprocess,
-        dev,
-        hydratable: true,
-        emitCss: true
-      }),
-      resolve({
-        browser: true,
-        dedupe
-      }),
-      commonjs(),
+			replace({
+				preventAssignment: true,
+				values:{
+					'process.browser': true,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
+			}),
+			svelte({
+        preprocess: preprocess,
+				compilerOptions: {
+					dev,
+					hydratable: true,
+				}
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/'
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			commonjs(),
 
-      legacy &&
-        babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
-          babelHelpers: "runtime",
-          exclude: ["node_modules/@babel/**"],
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: "> 0.25%, not dead"
-              }
-            ]
-          ],
-          plugins: [
-            "@babel/plugin-syntax-dynamic-import",
-            [
-              "@babel/plugin-transform-runtime",
-              {
-                useESModules: true
-              }
-            ]
-          ]
-        }),
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				babelHelpers: 'runtime',
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
 
-      !dev &&
-        terser({
-          module: true
-        })
-    ],
+			!dev && terser({
+				module: true
+			})
+		],
 
-    onwarn
-  },
+		preserveEntrySignatures: false,
+		onwarn,
+	},
 
-  server: {
-    input: config.server.input(),
-    output: config.server.output(),
-    plugins: [
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
       typescript(),
-      replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode)
-      }),
-      svelte({
-        preprocess,
-        generate: "ssr",
-        dev
-      }),
-      resolve({
-        dedupe
-      }),
-      commonjs()
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules ||
-        Object.keys(process.binding("natives"))
-    ),
+			replace({
+				preventAssignment: true,
+				values:{
+					'process.browser': false,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
+			}),
+			svelte({
+        preprocess: preprocess,
+				compilerOptions: {
+					dev,
+					generate: 'ssr',
+					hydratable: true,
+				},
+				emitCss: false
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/',
+				emitFiles: false // already emitted by client build
+			}),
+			resolve({
+				dedupe: ['svelte']
+			}),
+			commonjs()
+		],
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		preserveEntrySignatures: 'strict',
+		onwarn,
+	},
 
-    onwarn
-  },
-
-  serviceworker: {
-    input: config.serviceworker.input(),
-    output: config.serviceworker.output(),
-    plugins: [
-      resolve(),
-      replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode)
-      }),
-      commonjs(),
-      !dev && terser()
-    ],
-
-    onwarn
-  }
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				preventAssignment: true,
+				values:{
+					'process.browser': true,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+		preserveEntrySignatures: false,
+		onwarn,
+	}
 };
