b910c14e8baacb698143ecabdbc842a94f8cfade
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-04-27 13:39:22 -0700

    generic api/ endpoint

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const apiProxy = createProxyMiddleware({
-  //target: "https://reddit.com",
-  router: async function(req) {
-    return "https://reddit.com"
+  router: function(req) {
+    let target = req.url.split('/')[2];
+    //console.log('target ', target)
+    return {
+      protocol: 'https:', host: target,
+    }
   },
   changeOrigin: true,
-  pathRewrite: {
-    "^/api": "" // strip "/api" from the URL
+  pathRewrite: function(path, req) {
+    //console.log('path', '/' + req.url.split('/').slice(3).join('/'));
+    return '/' + req.url.split('/').slice(3).join('/')
   },
+
   onProxyRes(proxyRes) {
     proxyRes.headers['x-added'] = 'foobar'; // add new header to response
     delete proxyRes.headers['x-removed']; // remove header from response
